6addbde0d74a126a1b8225eb9f22eeb734bb00d2
Rob Wright <rob.wright@groupgti.com>
2017-09-13 11:23:24 +0100

    Working with events in directives: listen for mouseenter / mouseleave and set css class

--- app/favorite.directive.ts
+++ app/favorite.directive.ts
@@ -1,10 +1,17 @@
-import { Directive, HostBinding, Input } from '@angular/core';
+import { Directive, HostBinding, Input, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[mwFavorite]'
 })
+
 export class FavoriteDirective {
+  
   @HostBinding('class.is-favorite') isFavorite = true;
+  @HostBinding('class.is-favorite-hovering') hovering = false;
+
+  @HostListener('mouseenter') onmouseenter() { this.hovering = true };
+  @HostListener('mouseleave') onmouseleave() { this.hovering = false };
+
   @Input() set mwFavorite(value) {
     this.isFavorite = value;
   }
