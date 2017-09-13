5341a153f92ca813151b8bd8f59fea1622b67de0
Rob Wright <rob.wright@groupgti.com>
2017-09-13 11:16:48 +0100

    Attribute directives: custom (passing input value from element attribute to directive)

--- app/favorite.directive.ts
+++ app/favorite.directive.ts
@@ -1,8 +1,15 @@
-import { Directive, HostBinding } from '@angular/core';
+import { Directive, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: '[mwFavorite]'
 })
 export class FavoriteDirective {
   @HostBinding('class.is-favorite') isFavorite = true;
+
+  // use a setter method to assign local variable using input value
+  // this is what we passed from the element attribute:
+  // [mwFavorite]="mediaItem.isFavorite"
+  @Input() set mwFavorite(value) { 
+    this.isFavorite = value 
+  }
 }
