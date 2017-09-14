6a42702ebd850adb1cbb08791b75f08faf1b233a
Rob Wright <rob.wright@groupgti.com>
2017-09-14 10:11:44 +0100

    Template driven forms

--- app/media-item-form.component.ts
+++ app/media-item-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'mw-media-item-form',
@@ -6,4 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['app/media-item-form.component.css']
 })
 export class MediaItemFormComponent {
+
+  onSubmit(mediaItem) {
+    console.log(mediaItem)
+  }
 }
