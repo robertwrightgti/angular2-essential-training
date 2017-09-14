149da1de92177f4dd055e9f2b91f9e201dff19c5
Rob Wright <rob.wright@groupgti.com>
2017-09-14 10:37:15 +0100

    Model driven forms

--- app/media-item-form.component.ts
+++ app/media-item-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'mw-media-item-form',
@@ -6,7 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['app/media-item-form.component.css']
 })
 export class MediaItemFormComponent {
+
+  form;
+  
+  ngOnInit() {
+    //define a model to represent the form
+    this.form = new FormGroup({
+      medium: new FormControl('Movies'),
+      name: new FormControl(''),
+      category: new FormControl(''),
+      year: new FormControl('')
+    });
+  }
+
+  //function to handle form submission
   onSubmit(mediaItem) {
-    console.log(mediaItem);
+    console.log(mediaItem)
   }
 }
