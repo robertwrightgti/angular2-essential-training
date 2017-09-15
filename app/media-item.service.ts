2f570691f96446c74826f0e5a175b254328e71bc
Rob Wright <rob.wright@groupgti.com>
2017-09-15 16:35:43 +0100

    Using URLSearchParams

--- app/media-item.service.ts
+++ app/media-item.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, URLSearchParams } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class MediaItemService {
   constructor(private http: Http) {}
 
-  get() {
-    return this.http.get('mediaitems')
+  get(medium) {
+    let searchParams = new URLSearchParams();
+    searchParams.append('medium', medium);
+
+    return this.http.get('mediaitems', { search: searchParams })
       .map(response => {
         return response.json().mediaItems;
       });
