7a3f29bb84ce415bd6d8a75524fa92199bafc873
Rob Wright <rob.wright@groupgti.com>
2017-11-20 10:23:57 +0000

    02_03b - Bootstrapping the module for the browser

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app',
-    template: '<h1>My app</h1>'
+    template: '<h1 style="color: white">My app</h1>'
 })
 export class AppComponent {}
