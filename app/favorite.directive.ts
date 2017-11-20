99c2af4af5228bd928903cfc6d4359775cc7901b
Rob Wright <rob.wright@groupgti.com>
2017-11-20 14:19:39 +0000

    03_06b - Working with events in directives03_05b - Using directive values

--- app/favorite.directive.ts
+++ app/favorite.directive.ts
@@ -5,11 +5,14 @@ import { Directive, HostBinding, Input } from '@angular/core';
 })
 export class FavoriteDirective {
 
+  //use input value to set internal property value
   @Input()
   set mwFavorite(value) {
     this.isFavorite = value;
   }
   
+  //bind to class property of parent element
+  //set class depending on value of internal property
   @HostBinding('class.is-favorite') 
   isFavorite = true;
 
