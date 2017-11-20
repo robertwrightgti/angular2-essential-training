fc66340929e3d8d32b8b7950eaa5cb58d3de60a8
Rob Wright <rob.wright@groupgti.com>
2017-11-20 10:27:44 +0000

    02_04b - The component selector : changing the name of the selector form "app" to "mw-app"

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app',
+    selector: 'mw-app',
     template: '<h1 style="color: white">My app</h1>'
 })
 export class AppComponent {}
