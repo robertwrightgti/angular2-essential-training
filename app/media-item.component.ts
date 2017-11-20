ff75e3c71aa104c5edc3f6356acf761104c7c197
Rob Wright <rob.wright@groupgti.com>
2017-11-20 13:14:06 +0000

    02_10b - Event binding

--- app/media-item.component.ts
+++ app/media-item.component.ts
@@ -5,10 +5,8 @@ import { Component } from '@angular/core';
   templateUrl: 'app/media-item.component.html',
   styleUrls: ['app/media-item.component.css']
 })
-export class MediaItemComponent {
-  name = "Hawk the Slayer";
-
-  wasWatched() {
-    return true;
+export class MediaItemComponent { 
+  onDelete() {
+    console.log("delete!")
   }
 }
