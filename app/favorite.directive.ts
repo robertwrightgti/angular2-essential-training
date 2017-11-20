d0b3af6986afebcfb01250765eaa5cf8c6632fe2
Rob Wright <rob.wright@groupgti.com>
2017-11-20 14:27:16 +0000

    03_06b - Working with events in directives

--- app/favorite.directive.ts
+++ app/favorite.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, HostBinding, Input } from '@angular/core';
+import { Directive, HostBinding, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[mwFavorite]'
@@ -11,9 +11,22 @@ export class FavoriteDirective {
     this.isFavorite = value;
   }
   
+  //listen for event on parent element (mouse enter and leave)
+  //set hovering property accordingly
+  @HostListener('mouseenter') onmouseenter() {
+    this.hovering = true;
+  }
+
+  @HostListener('mouseleave') onmouseleave() {
+    this.hovering = false;
+  }
+
   //bind to class property of parent element
-  //set class depending on value of internal property
+  //set class depending on value of internal properties
   @HostBinding('class.is-favorite') 
   isFavorite = true;
 
+  @HostBinding('class.is-favorite-hovering')
+  hovering = false;
+  
 }
