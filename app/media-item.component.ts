ba0f0fbd5fda844bd694ee366d926067c96a7a2d
Rob Wright <rob.wright@groupgti.com>
2017-11-20 13:29:07 +0000

    02_11b - Getting data to the component with Input

--- app/media-item.component.ts
+++ app/media-item.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'mw-media-item',
@@ -6,6 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['app/media-item.component.css']
 })
 export class MediaItemComponent { 
+
+  //use @Input() decorator rather than inputs[]
+  //advantage: you can define the type and accessibility
+  @Input() 
+  public mediaItem: any;
+
   onDelete() {
     console.log("delete!")
   }
